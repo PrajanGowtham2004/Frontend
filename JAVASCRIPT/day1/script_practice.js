87731351ccb15997904f8b4b2c8175dff4871e3f
Prajan Gowtham <prajangowtham2004@gmail.com>
2026-06-08 01:15:56 +0530

    js updated

---
+++ JAVASCRIPT/day1/script_practice.js
@@ -0,0 +1,180 @@
+var nameVar = "VAR 20 TIMES";
+console.log(nameVar);
+var firstName = "Prajan";
+console.log(firstName);
+var middleName = "Gowtham";
+console.log(middleName);
+var lastName = "S"
+console.log(lastName);
+var fullName = firstName + " " + middleName + " " + lastName;
+console.log(fullName);
+var age = 21;
+console.log(age);
+var isStudent = true;
+console.log(isStudent);
+var hobbies = ["coding", "gaming", "traveling"];
+console.log(hobbies);
+var gender = "Male";
+console.log(gender);
+var country = "India";
+console.log(country); 
+var city = "Chennai";
+console.log(city);
+var profession = "Web Developer";
+console.log(profession);
+var course = "SLA";
+console.log(course);
+var university = "SRM University";
+console.log(university);
+var phoneNumber = "8825626967";
+console.log(phoneNumber);
+var email = "prajangowtham2004@gmail.com";
+console.log(email);
+var address = "Bhoomi PG, Ramapuram, Chennai";
+console.log(address);
+var zipCode = "600089";
+console.log(zipCode);
+var state = "Tamil Nadu";
+console.log(state);
+var language = "Tamil";
+console.log(language);
+
+
+
+
+
+
+
+
+
+
+let nameLet = "LET 30 TIMES";
+console.log(nameLet);
+let firstNameLet = "Prajan";
+console.log(firstNameLet);
+let middleNameLet = "Gowtham";
+console.log(middleNameLet);
+let lastNameLet = "S";
+console.log(lastNameLet);
+let fullNameLet = firstNameLet + " " + middleNameLet + " " + lastNameLet;
+console.log(fullNameLet);
+let ageLet = 21;
+console.log(ageLet);
+let isStudentLet = true;
+console.log(isStudentLet);
+let hobbiesLet = ["coding", "gaming", "traveling"];
+console.log(hobbiesLet);
+let genderLet = "Male";
+console.log(genderLet);
+let countryLet = "India";
+console.log(countryLet);
+let cityLet = "Chennai";
+console.log(cityLet);
+let professionLet = "Web Developer";
+console.log(professionLet);
+let courseLet = "SLA";
+console.log(courseLet);
+let universityLet = "SRM University";
+console.log(universityLet);
+let phoneNumberLet = "8825626967";
+console.log(phoneNumberLet);
+let emailLet = "prajangowtham2004@gmail.com";
+console.log(emailLet);
+let addressLet = "Bhoomi PG, Ramapuram, Chennai";
+console.log(addressLet);
+let zipCodeLet = "600089";
+console.log(zipCodeLet);
+let stateLet = "Tamil Nadu";
+console.log(stateLet);
+let languageLet = "Tamil";
+console.log(languageLet);
+let isEmployedLet = false;
+console.log(isEmployedLet);
+let salaryLet = 0;
+console.log(salaryLet);
+let companyLet = "None";
+console.log(companyLet);
+let experienceLet = 0;
+console.log(experienceLet);
+let skillsLet = ["HTML", "CSS", "JavaScript"];
+console.log(skillsLet);
+let certificationsLet = ["SLA Certification"];
+console.log(certificationsLet);
+let projectsLet = ["Portfolio Website"];
+console.log(projectsLet);
+let githubLinkLet = "https://github.com/prajangowtham";
+console.log(githubLinkLet);
+let linkedinLinkLet = "https://www.linkedin.com/in/prajan-gowtham-s-123456789/";
+console.log(linkedinLinkLet);
+let cvLinkLet = "https://drive.google.com/file/d/1a2b3c4d5e6f7g8h9i0j/view?usp=sharing";
+console.log(cvLinkLet);
+
+
+
+
+
+
+
+
+
+
+const nameConst = "CONST 30 TIMES";
+console.log(nameConst);
+const firstNameConst = "Prajan";
+console.log(firstNameConst);
+const middleNameConst = "Gowtham";
+console.log(middleNameConst);
+const lastNameConst = "S";
+console.log(lastNameConst);
+const fullNameConst = firstNameConst + " " + middleNameConst + " " + lastNameConst;
+console.log(fullNameConst);
+const ageConst = 21;
+console.log(ageConst);
+const isStudentConst = true;
+console.log(isStudentConst);
+const hobbiesConst = ["coding", "gaming", "traveling"];
+console.log(hobbiesConst);
+const genderConst = "Male";
+console.log(genderConst);
+const countryConst = "India";
+console.log(countryConst);
+const cityConst = "Chennai";
+console.log(cityConst);
+const professionConst = "Web Developer";
+console.log(professionConst);
+const courseConst = "SLA";
+console.log(courseConst);
+const universityConst = "SRM University";
+console.log(universityConst);
+const phoneNumberConst = "8825626967";
+console.log(phoneNumberConst);
+const emailConst = "prajangowtham2004@gmail.com";
+console.log(emailConst);
+const addressConst = "Bhoomi PG, Ramapuram, Chennai";
+console.log(addressConst);
+const zipCodeConst = "600089";
+console.log(zipCodeConst);
+const stateConst = "Tamil Nadu";
+console.log(stateConst);
+const languageConst = "Tamil";
+console.log(languageConst);
+const isEmployedConst = false;
+console.log(isEmployedConst);
+const salaryConst = 0;
+console.log(salaryConst);
+const companyConst = "None";
+console.log(companyConst);
+const experienceConst = 0;
+console.log(experienceConst);
+const skillsConst = ["HTML", "CSS", "JavaScript"];
+console.log(skillsConst);
+const certificationsConst = ["SLA Certification"];
+console.log(certificationsConst);
+const projectsConst = ["Portfolio Website"];
+console.log(projectsConst);
+const githubLinkConst = "https://github.com/prajangowtham";
+console.log(githubLinkConst);
+const linkedinLinkConst = "https://www.linkedin.com/in/prajan-gowtham-s-123456789/";
+console.log(linkedinLinkConst);
+const cvLinkConst = "https://drive.google.com/file/d/1a2b3c4d5e6f7g8h9i0j/view?usp=sharing";
+console.log(cvLinkConst);
