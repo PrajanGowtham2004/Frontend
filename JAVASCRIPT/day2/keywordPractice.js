28d243e8a0a3b48041ac71940b380cd073efda42
Prajan Gowtham <prajangowtham2004@gmail.com>
2026-06-08 23:47:22 +0530

    updated day2

---
+++ JAVASCRIPT/day2/keywordPractice.js
@@ -0,0 +1,187 @@
+var nameVar = "IN VAR";
+console.log(typeof nameVar,nameVar);
+var firstName = "Prajan";
+console.log(typeof firstName,firstName);
+var middleName = "Gowtham";
+console.log(typeof middleName,middleName);
+var lastName = "S";
+console.log(typeof lastName,lastName);
+var fullName = "Prajan Gowtham S";
+console.log(typeof fullName,fullName);
+var age = 21;
+console.log(typeof age,age);
+var isStudent = true;
+console.log(typeof isStudent,isStudent);
+var hobbies = "Travelling";
+console.log(typeof hobbies,hobbies);
+var gender = "Male";
+console.log(typeof gender,gender);
+var country = "India";
+console.log(typeof country,country);
+var city = "Chennai";
+console.log(typeof city,city);
+var profession = "Web Developer";
+console.log(typeof profession,profession);
+var course = "Full Stack Web Development";
+console.log(typeof course,course);
+var university = "SRM University";
+console.log(typeof university,university);
+var phoneNumber = "1234567890";
+console.log(typeof phoneNumber,phoneNumber);
+var email = "prajan.gowtham.s@example.com";
+console.log(typeof email,email);
+var address = "123 Main Street, Chennai";
+console.log(typeof address,address);
+var zipCode = "600089";
+console.log(typeof zipCode,zipCode);
+var state = "Tamil Nadu";
+console.log(typeof state,state);
+var language = "Tamil";
+console.log(typeof language,language);
+var user = null;
+console.log(typeof user, user); 
+
+
+
+
+
+
+
+
+
+
+let nameLet = "IN LET";
+console.log(typeof nameLet,nameLet);
+let firstNameLet = "Prajan";
+console.log(typeof firstNameLet,firstNameLet);
+let middleNameLet = "Gowtham";
+console.log(typeof middleNameLet,middleNameLet);
+let lastNameLet = "S";
+console.log(typeof lastNameLet,lastNameLet);
+let fullNameLet = "Prajan Gowtham S";
+console.log(typeof fullNameLet,fullNameLet);
+let ageLet = 21;
+console.log(typeof ageLet,ageLet);
+let isStudentLet = true;
+console.log(typeof isStudentLet,isStudentLet);
+let hobbiesLet = "Travelling";
+console.log(typeof hobbiesLet,hobbiesLet);
+let genderLet = "Male";
+console.log(typeof genderLet,genderLet);
+let countryLet = "India";
+console.log(typeof countryLet,countryLet);
+let cityLet = "Chennai";
+console.log(typeof cityLet,cityLet);
+let professionLet = "Web Developer";
+console.log(typeof professionLet,professionLet);
+let courseLet = "Full Stack Web Development";
+console.log(typeof courseLet,courseLet);
+let universityLet = "SRM University";
+console.log(typeof universityLet,universityLet);
+let phoneNumberLet = "1234567890";
+console.log(typeof phoneNumberLet,phoneNumberLet);
+let emailLet = "prajan.gowtham.s@example.com";
+console.log(typeof emailLet,emailLet);
+let addressLet = "123 Main Street, Chennai";
+console.log(typeof addressLet,addressLet);
+let zipCodeLet = "600089";
+console.log(typeof zipCodeLet,zipCodeLet);
+let stateLet = "Tamil Nadu";
+console.log(typeof stateLet,stateLet);
+let languageLet = "Tamil";
+console.log(typeof languageLet,languageLet);
+let isEmployedLet = false;
+console.log(typeof isEmployedLet,isEmployedLet);
+let salaryLet = 50000;
+console.log(typeof salaryLet,salaryLet);
+let companyLet = "ABC Corp";
+console.log(typeof companyLet,companyLet);
+let experienceLet = 2;
+console.log(typeof experienceLet,experienceLet);
+let skillsLet = ["JavaScript", "Python"];
+console.log(typeof skillsLet,skillsLet);
+let certificationsLet = "Certified JavaScript Developer";
+console.log(typeof certificationsLet,certificationsLet);
+let projectsLet = "E-commerce Website";
+console.log(typeof projectsLet,projectsLet);
+let githubLinkLet = "https://github.com/prajan";
+console.log(typeof githubLinkLet,githubLinkLet);
+let linkedinLinkLet = "https://linkedin.com/in/prajan";
+console.log(typeof linkedinLinkLet,linkedinLinkLet);
+let cvLinkLet = "https://example.com/cv.pdf";
+console.log(typeof cvLinkLet,cvLinkLet);
+let userLet = null;
+console.log(typeof userLet, userLet); 
+
+
+
+
+
+
+
+
+
+
+
+const nameConst = "IN CONST";
+console.log(typeof nameConst,nameConst);
+const firstNameConst = "Prajan";
+console.log(typeof firstNameConst,firstNameConst);
+const middleNameConst = "Gowtham";
+console.log(typeof middleNameConst,middleNameConst);
+const lastNameConst = "S";
+console.log(typeof lastNameConst,lastNameConst);
+const fullNameConst = "Prajan Gowtham S";
+console.log(typeof fullNameConst,fullNameConst);
+const ageConst = 21;
+console.log(typeof ageConst,ageConst);
+const isStudentConst = true;
+console.log(typeof isStudentConst,isStudentConst);
+const hobbiesConst = "Travelling";
+console.log(typeof hobbiesConst,hobbiesConst);
+const genderConst = "Male";
+console.log(typeof genderConst,genderConst);
+const countryConst = "India";
+console.log(typeof countryConst,countryConst);
+const cityConst = "Chennai";
+console.log(typeof cityConst,cityConst);
+const professionConst = "Web Developer";
+console.log(typeof professionConst,professionConst);
+const courseConst = "Full Stack Web Development";
+console.log(typeof courseConst,courseConst);
+const universityConst = "SRM University";
+console.log(typeof universityConst,universityConst);
+const phoneNumberConst = "1234567890";
+console.log(typeof phoneNumberConst,phoneNumberConst);
+const emailConst = "prajan.gowtham.s@example.com";
+console.log(typeof emailConst,emailConst);
+const addressConst = "123 Main Street, Chennai";
+console.log(typeof addressConst,addressConst);
+const zipCodeConst = "600089";
+console.log(typeof zipCodeConst,zipCodeConst);
+const stateConst = "Tamil Nadu";
+console.log(typeof stateConst,stateConst);
+const languageConst = "Tamil";
+console.log(typeof languageConst,languageConst);
+const isEmployedConst = false;
+console.log(typeof isEmployedConst,isEmployedConst);
+const salaryConst = 50000;
+console.log(typeof salaryConst,salaryConst);
+const companyConst = "ABC Corp";
+console.log(typeof companyConst,companyConst);
+const experienceConst = 2;
+console.log(typeof experienceConst,experienceConst);
+const skillsConst = ["JavaScript", "Python"]    ;
+console.log(typeof skillsConst,skillsConst);
+const certificationsConst = "Certified JavaScript Developer";
+console.log(typeof certificationsConst,certificationsConst);
+const projectsConst = "E-commerce Website";
+console.log(typeof projectsConst,projectsConst);
+const githubLinkConst = "https://github.com/prajan";
+console.log(typeof githubLinkConst,githubLinkConst);
+const linkedinLinkConst = "https://linkedin.com/in/prajan";
+console.log(typeof linkedinLinkConst,linkedinLinkConst);
+const cvLinkConst = "https://example.com/cv.pdf";
+console.log(typeof cvLinkConst,cvLinkConst);
+const userConst = null;
+console.log(typeof userConst, userConst); 
